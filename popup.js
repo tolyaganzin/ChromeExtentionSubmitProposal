e2a65aae8312e1f627f70b293efc98b677296750
tolyaganzin <tolyaganzin@mail.ru>
2017-02-16 18:09:04 +0200

    add save and get from local storage

--- popup.js
+++ popup.js
@@ -1,7 +1,10 @@
 /* A function creator for callbacks */
+var urlpage = '';
+  // chrome.storage.local.clear();
 function doStuffWithDOM(element) {
-  console.log(element);
+
   $('h6').addClass("hide");
+
   for (var i = 0; i < element.length; i++) {
     var preElement = $('pre.hide').clone();
 
@@ -10,6 +13,24 @@ function doStuffWithDOM(element) {
 
     $('div.content').append($(preElement));
   }
+
+  chrome.storage.local.get(["proposals"], function(items){
+    var arr = [];
+    if (items.proposals) {
+      if(!$.grep(items.proposals, function(e){ return e.urlpage == $(urlpage).text() }).length) {
+        arr = items.proposals;
+        arr.push({'urlpage': $(urlpage).text(), 'questions': element});
+        console.log(arr);
+        chrome.storage.local.set({ "proposals": arr}, function(){});
+      } else {
+        console.log("has this proposals");
+      }
+    } else {
+      arr.push({'urlpage': $(urlpage).text(), 'questions': element});
+      console.log(arr);
+      chrome.storage.local.set({ "proposals": arr}, function(){});
+    }
+  });
 }
 function toDoPizdato() {
 
@@ -21,8 +42,8 @@ function toDoPizdato() {
 
       $('div.content').removeClass("hide");
       $('div.content').empty();
-      var url = $('#bidName').clone();
-      $('div.content').append($(url).text(tab.url).attr("href", tab.url));
+      urlpage = $('#bidName').clone();
+      $('div.content').append($(urlpage).text(tab.url).attr("href", tab.url));
 
       chrome.tabs.sendMessage(tab.id, { text: "report_back" }, doStuffWithDOM);
     });
@@ -36,6 +57,36 @@ function onWindowLoad() {
   $('#find').click(function() {
     $('div.content').removeClass("hide");
     $('div.content').empty();
+
+    chrome.tabs.getSelected(null,function(tab) {
+      chrome.storage.local.get(["proposals"], function(items){
+        if (items.proposals) {
+          if($.grep(items.proposals, function(e){ return e.urlpage == tab.url }).length) {
+            urlpage = $('#bidName').clone();
+            $('div.content').append($(urlpage).text(tab.url).attr("href", tab.url));
+
+            $.grep(items.proposals, function(e){
+               if(e.urlpage == tab.url) {
+                 for (var i = 0; i < e.questions.length; i++) {
+                   var preElement = $('pre.hide').clone();
+
+                   $(preElement).removeClass("hide");
+                   $(preElement).text(e.questions[i]);
+
+                   $('div.content').append($(preElement));
+                 }
+               }
+            });
+            
+          }
+
+        } else {
+          urlpage = $('#bidName').clone();
+          $('div.content').append($(urlpage).text(empty).attr("href", '#'));
+        }
+      });
+
+    });
   })
 
 }
