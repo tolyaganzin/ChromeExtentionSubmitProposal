e61aee329109e708112fdbcf30e494ecfcb9c69d
tolyaganzin <tolyaganzin@mail.ru>
2017-02-16 17:13:22 +0200

    add styles

---
+++ README.md
@@ -0,0 +1,1 @@
+#Chrome Extention Submit Proposal
--- popup.js
+++ popup.js
@@ -1,31 +1,43 @@
+/* A function creator for callbacks */
+function doStuffWithDOM(element) {
+  console.log(element);
+  $('h6').addClass("hide");
+  for (var i = 0; i < element.length; i++) {
+    var preElement = $('pre.hide').clone();
+
+    $(preElement).removeClass("hide");
+    $(preElement).text(element[i]);
+
+    $('div.content').append($(preElement));
+  }
+}
+function toDoPizdato() {
+
+  $('h6.hide').removeClass("hide");
+
+  chrome.tabs.getSelected(null,function(tab) {
+    ////auto get test
+    $.get(tab.url, function( htmlCurrentPage ) {
+
+      $('div.content').removeClass("hide");
+      $('div.content').empty();
+      var url = $('#bidName').clone();
+      $('div.content').append($(url).text(tab.url).attr("href", tab.url));
+
+      chrome.tabs.sendMessage(tab.id, { text: "report_back" }, doStuffWithDOM);
+    });
+  });
+}
 function onWindowLoad() {
 
-  /* A function creator for callbacks */
-  function doStuffWithDOM(element) {
-    console.log(element);
-    for (var i = 0; i < element.length; i++) {
-      $('div.content').append($('<pre></pre>').text(element[i]));
-    }
-  }
   $('#save').click(function() {
     toDoPizdato();
   })
+  $('#find').click(function() {
+    $('div.content').removeClass("hide");
+    $('div.content').empty();
+  })
 
-  function toDoPizdato() {
-
-    chrome.tabs.getSelected(null,function(tab) {
-      console.log('teb', tab);
-
-      ////auto get test
-      $.get(tab.url, function( htmlCurrentPage ) {
-        var tmp = $(htmlCurrentPage).find("h1.m-0-top").text().trim();
-
-        $('#bidName').text($(htmlCurrentPage).find("label.ng-binding").first().text()).attr("href", tab.url);
-
-        chrome.tabs.sendMessage(tab.id, { text: "report_back" }, doStuffWithDOM);
-      });
-    });
-  }
 }
 
 window.onload = onWindowLoad;
